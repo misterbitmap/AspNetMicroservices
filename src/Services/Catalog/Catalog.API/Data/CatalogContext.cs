a2a779b94b643bbb84ec664ef396a9020a6cd9a1
misterbitmap <ramezani.bist@gmail.com>
2024-01-16 14:07:00 +0330

    adding mongodb and repository for catalog

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionSettings"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+            Products = database.GetCollection<Product>
+                (configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products); 
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
